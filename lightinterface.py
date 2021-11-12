7951f014fbbe981540166ff0867986abdf99c99d
Aidan Gomez <44831139+atomdog@users.noreply.github.com>
2021-11-11 22:32:59 -0500

    Create lightinterface.py

---
+++ lightinterface.py
@@ -0,0 +1,22 @@
+import time
+
+import zmq
+from zmq.asyncio import Context, Poller
+import asyncio
+
+
+async def sender(blocka, blockb, blockc, blockd):
+
+    ctx = Context.instance()
+    print("<PUSHING SERVER...>")
+    url = 'tcp://127.0.0.1:5555'
+    tic = time.time()
+    push = ctx.socket(zmq.PUSH)
+    push.bind(url)
+
+    combined = str(blocka)+"x"+str(blockb)+"x"+str(blockc)+"x"+str(blockd)
+    await push.send_multipart([str(combined).encode('ascii')])
+    await asyncio.sleep(0.1)
+
+def handler(blocka, blockb, blockc, blockd):
+    asyncio.run(sender(blocka, blockb, blockc, blockd))
